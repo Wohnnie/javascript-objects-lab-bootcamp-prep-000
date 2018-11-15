9b78af437b3feb5f1cb93da08da862ccdba1b868
Angie Quin <aquinones100@gmail.com>
2018-11-15 17:33:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,15 +4,7 @@ function updateObjectWithKeyAndValue(obj, key, value) {
   return Object.assign({}, object, {[key]: value});
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(recipes, mac, cheese) {
-  recipes.mac = ["milk"];
-  return recipes;
-}
-
-function deleteFromObjectByKey(recipes, mac) {
-  return delete recipes.mac;
-}
-
-function destructivelyDeleteFromObjectByKey(recipes, mac) {
-  return delete recipes[mac];
+function destructivelyUpdateObjectWithKeyAndValue (obj, key, value){
+  obj[key] = value;
+  return obj
 }
